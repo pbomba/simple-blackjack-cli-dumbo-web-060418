1cf0b7e795e879c6a2c0634de1979a8c334b5a24
Paul Bomba <paul@paulbomba.com>
2018-05-24 19:35:13 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,57 @@
+hand = []
+
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  newcard = 1 + rand(11)
+  return newcard
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(total)
+  puts "Your cards add up to #{total}"
+  return total
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  dec = gets.chomp
+  return dec
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  hand_total = deal_card + deal_card
+  display_card_total(hand_total)
+  return hand_total
 end
 
-def hit?
-  # code hit? here
+def hit?(number)
+  prompt_user
+  user_input = get_user_input
+  
+  until user_input == "h" || "s"
+  invalid_command
+  prompt_user
+  user_input = get_user_input
+  end
+  
+  if user_input == "h"
+    number += deal_card
+  elsif user_input == "s"
+    number
+  end
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter an 'h' or an 's'"
 end
 
 #####################################################
@@ -39,6 +59,13 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  number = initial_round
+  until number > 21
+  hit?(number)
+  display_card_total(number)
+  end
+end_game(number)
+  
 end
     
